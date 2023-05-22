ed5c057a1181301ed559fcfee7bb1b3bbbfd58f5
pieroMejiaR <pmejia@unsa.edu.pe>
2023-05-22 10:58:06 -0500

    Terminando ejercicio 5

--- lab02/Ejercicio5/index.js
+++ lab02/Ejercicio5/index.js
@@ -1,14 +1,49 @@
-function crearTabla(cantidad) {
-    for (i=0; i < cantidad.value; i++) {
-        document.getElementById("tabla").innerHTML =`
-        <tr>
-            <td>{i}</td>
-        </tr>`;
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    var cantidadValores = document.getElementById("cantidad").value;
+    
+    crearTabla(cantidadValores);
+    document.getElementById("calcular-btn").disabled = false;
+  });
+  
+  function crearTabla(cantidadFilas) {
+    var tablaContainer = document.getElementById("tabla-container");
+    tablaContainer.innerHTML = ""; // Limpiar contenido anterior
+    
+    var tabla = document.createElement("table");
+    tabla.setAttribute("id", "tabla");
+    
+    for (var i = 0; i < cantidadFilas; i++) {
+      var fila = document.createElement("tr");
+      
+      var celdaNumero = document.createElement("td");
+      var numeroInput = document.createElement("input");
+      numeroInput.setAttribute("type", "number");
+      celdaNumero.appendChild(numeroInput);
+      
+      fila.appendChild(celdaNumero);
+      tabla.appendChild(fila);
+    }
+    
+    tablaContainer.appendChild(tabla);
+    
+    // Crear botón de calcular suma
+    var calcularBtn = document.createElement("button");
+    calcularBtn.setAttribute("id", "calcular-btn");
+    calcularBtn.textContent = "Calcular suma";
+    calcularBtn.disabled = true;
+    tablaContainer.appendChild(calcularBtn);
+    
+    calcularBtn.addEventListener("click", function() {
+      var valores = document.querySelectorAll("#tabla input");
+      var suma = 0;
+      
+      for (var i = 0; i < valores.length; i++) {
+        suma += parseInt(valores[i].value);
+      }
+      
+      var resultado = document.getElementById("resultado");
+      resultado.textContent = "La suma de los valores es: " + suma;
+    });
   }
-
-    document.getElementById("tabla").innerHTML = `<button onclick= sumar()>Sumar</button>`;
-}
-
-function sumar() {
-        
-}
