2d5045f13ee0a360a17b929608b1a4363cc209d2
-- <-->
2023-06-06 14:23:24 -0500

    Completando Ejercicio2d

--- lab04/Ejercicio2d.py
+++ lab04/Ejercicio2d.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+white_space = Picture(SQUARE)
+black_space = white_space.negative()
+
+board = white_space.join(black_space).horizontalRepeat(4)
+
+draw(board)
