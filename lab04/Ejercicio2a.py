c873eeddef912ef2bcccb8a3129dba4c72e28696
pieroMejiaR <pmejia@unsa.edu.pe>
2023-06-06 11:09:15 -0500

    Completando el ejercicio 2a

--- lab04/Ejercicio2a.py
+++ lab04/Ejercicio2a.py
@@ -1,5 +1,12 @@
 from chessPictures import *
 from interpreter import draw
+from pieces import *
+from picture import *
 
 
-draw(queen)
+white_knight = Picture(KNIGHT)
+black_knight = white_knight.negative()
+
+board_knights = white_knight.join(black_knight).up(black_knight.join(white_knight))
+
+draw(board_knights)
