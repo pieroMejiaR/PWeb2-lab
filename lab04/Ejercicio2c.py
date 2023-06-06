46a08492ea49c2a30915eb92d8e99ab778ff58fc
-- <-->
2023-06-06 14:11:47 -0500

    Completando Ejercicio2c

--- lab04/Ejercicio2c.py
+++ lab04/Ejercicio2c.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+white_queen = Picture(QUEEN)
+
+board_queens = white_queen.join(white_queen).join(white_queen).join(white_queen)
+
+draw(board_queens)
