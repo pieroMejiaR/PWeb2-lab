8f1f8b9666a51883353e8511934c1a176c3efd86
-- <-->
2023-06-06 14:06:30 -0500

    Completando Ejercicio2b

--- lab04/Ejercicio2b.py
+++ lab04/Ejercicio2b.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+white_knight = Picture(KNIGHT)
+black_knight = white_knight.negative()
+
+board_knights = white_knight.join(black_knight).up(black_knight.verticalMirror().join(white_knight.verticalMirror()))
+
+draw(board_knights)
